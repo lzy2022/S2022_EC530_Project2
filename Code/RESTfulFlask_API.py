d9849addfa8c0dd35ff1029363497805bb866dc3
lzy2022 <lzy2022@users.noreply.github.com>
2022-02-28 11:20:25 -0500

    Add a basic API

--- Code/RESTfulFlask_API.py
+++ Code/RESTfulFlask_API.py
@@ -1,10 +1,25 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from db_info import DB_acc_info
+import random
 
 app = Flask(__name__)
 api = Api(app)
 
-class H(Resource):
+db_addr = './DB/Project_2.db'
+session_list = {}
+session_list[0] = DB_acc_info(db_addr)
+
+def get_DB_Acc_session(session_id):
+    return session_list[session_id]
+
+def new_DB_Acc_session(u_id, pw):
+    session_id = random.randint(1000, 3000)
+    session_list[session_id] = DB_acc_info(db_addr)
+    session_list[session_id].user_login(u_id, pw)
+    return session_id
+
+class User_Login(Resource):
     def get(self):
         return {'msg': "Hello"}
     
--- Code/api_Test.py
+++ Code/api_Test.py
@@ -1,4 +1,3 @@
-from urllib import request
 import requests
 
 BASE = "http://127.0.0.1:5000/"
